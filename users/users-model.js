38c3d4b8aef1bf899a2e66785060eec2b6e2260c
andrewhpark25 <andrewhpark25@gmail.com>
2020-05-25 23:40:51 -0700

    push

--- users/users-model.js
+++ users/users-model.js
@@ -9,7 +9,7 @@ module.exports = {
 };
 
 function find() {
-  return db("users").select("id", "username");
+  return db("users").select("id", "username").orderBy("id");
 }
 
 function findBy(filter) {
